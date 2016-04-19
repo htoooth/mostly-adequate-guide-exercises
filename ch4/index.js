69cfc48679fee0d48153485a3b78b5fe1f773848
htoooth <ht.anglenx@gmail.com>
2016-04-19 15:45:36 +0800

    uupdate ch4

--- ch4/index.js
+++ ch4/index.js
@@ -6,19 +6,26 @@ let curry = _.curry
 // exercise 1
 let words = _.split(' ')
 
-// exercise 2
+// exercise 1a 
 
 let sentences = _.map(words)
 
-// exercise 3
+// exercise 2 
 // 
 
 let filterQs = _.filter(_.match(/q/i));
 
-// exercise 4
+// exercise 3 
 
 let _keepHighest = (x, y) => x >= y ? x : y
 
 let max = xs => _.reduce((acc, x) => _keepHighest(acc, x), -Infinity, xs)
-
 let max2 = _.reduce(_keepHighest, -Infinity)
+
+// Bonus 1
+
+let slice = _.curry((start, end, xs) => xs.slice(start, end))
+
+// Bonus 2
+
+let take = slice(0)
